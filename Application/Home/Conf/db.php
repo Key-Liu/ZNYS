a8668a203f5ecfd4f6696b5196105cc1d9de6713
Key <lj.key.scut@gmail.com>
2016-03-09 18:28:30 +0800

    修改数据库设置

--- Application/Home/Conf/db.php
+++ Application/Home/Conf/db.php
@@ -3,11 +3,11 @@ return array(
 	//'配置项'=>'配置值'
 	// 添加数据库配置信息
 	'DB_TYPE'=>'mysql',// 数据库类型
-	'DB_HOST'=>'127.0.0.1',// 服务器地址
-	'DB_NAME'=>'ZNYS',// 数据库名
-	'DB_USER'=>'root',// 用户名
-	'DB_PWD'=>'ky1995.9.26',// 密码
-	'DB_PORT'=>3306,// 端口
+	'DB_HOST'=>SAE_MYSQL_HOST_M,// 服务器地址
+	'DB_NAME'=>SAE_MYSQL_DB,// 数据库名
+	'DB_USER'=>SAE_MYSQL_USER,// 用户名
+	'DB_PWD'=>SAE_MYSQL_PASS,// 密码
+	'DB_PORT'=>SAE_MYSQL_PORT,// 端口
 	'DB_CHARSET'=>'utf8',// 数据库字符集
 	'DB_PREFIX'=>'',
 );
